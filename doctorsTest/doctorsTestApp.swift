15a9b6317aaffcc56aadec9d033728d0c7a04668
Sergey Vidishev <ft.priest@gmail.com>
2024-08-16 20:42:23 +0300

    Initial Commit

---
+++ doctorsTest/doctorsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  doctorsTestApp.swift
+//  doctorsTest
+//
+//  Created by Сергей on 16.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct doctorsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
